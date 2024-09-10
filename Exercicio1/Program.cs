54d016463e655319afbf7288d71cd40dc5eb8416
cadu0794 <cedusantos10@gmail.com>
2024-09-10 11:55:46 -0300

    Adicionar arquivos de projeto.

---
+++ Exercicio1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 12, soma = 0, k = 1;
+
+            while (k < i)
+            {
+                k = k + 1;
+                soma = soma + k;
+            }
+
+            Console.WriteLine("###############");
+            Console.WriteLine("O total da soma é: " + soma);
+            Console.WriteLine("###############");
+            Console.ReadLine();
+        }
+    }
+}
